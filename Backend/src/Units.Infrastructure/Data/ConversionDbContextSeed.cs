324da1f820184429900f860c7ec3835e0a8b7948
Toby2xl <tobyononaku@gmail.com>
2023-08-12 00:11:44 +0100

    Added Integration and UnitTests

---
+++ Backend/src/Units.Infrastructure/Data/ConversionDbContextSeed.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Units.Infrastructure.Data;
+
+public static class ConversionDbContextSeed
+{
+    public static async Task SeedConversionDataAsync(ConversionDbContext context)
+    {
+        if(!context.Conversions.Any())
+        {
+    
+        }
+    }
+}
