6e9dff917d2d4eeafbde016686fc3bbea6ea9c23
Toby2xl <tobyononaku@gmail.com>
2023-08-13 04:02:24 +0100

    fixed docker build

--- Backend/src/Units.Api/Extension/MigrationExtensions.cs
+++ Backend/src/Units.Api/Extension/MigrationExtensions.cs
@@ -12,7 +12,16 @@ namespace Units.Api.Extension
             using var scope = app.Services.CreateScope();
             var dbContext = scope.ServiceProvider.GetRequiredService<ConversionDbContext>();
             
-            dbContext.Database.Migrate();
+            if(dbContext.Database.GetPendingMigrations().Any())
+            {
+                app.Logger.LogInformation("-------Applying pending migrstions.....");
+                dbContext.Database.Migrate();
+                app.Logger.LogInformation("------Database successfully migrated...");
+            }
+            else
+            {
+                app.Logger.LogInformation("---->>There are no pending migrations.....");
+            }
             //seed conversion rates
             ConversionDbContextSeed.SeedConversionDataAsync(dbContext);
 
